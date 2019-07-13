9ab668ab2fe012ff47e81a74332f3c3bc8a581cb
Gulaiym <gulaiym.kalieva@gmail.com>
2019-07-12 22:30:57 -0500

    Modified Version

--- src/test/java/MgmMatrix/Gulaiym.java
+++ src/test/java/MgmMatrix/Gulaiym.java
@@ -3,7 +3,7 @@ package MgmMatrix;
 import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;

b0fd3dfa6497aaad951d48efcb4902eb3fe72f68
oleksandrivanyshyn <alexastucco@gmail.com>
2019-07-12 21:41:45 -0500

    Oleksandr added

--- src/test/java/Oleksandr.java
+++ src/test/java/Oleksandr.java
@@ -11,6 +11,75 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class Oleksandr {
+    static WebDriver driver;
 
-    
+    @BeforeMethod
+    public void setUp() {
+        //Test 1;
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void test1(){
+        Assert.assertTrue(image1(driver));
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertTrue(image2(driver));
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertTrue(image3(driver));
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+    public static boolean image1(WebDriver driver) {
+        driver.get("https://www.mgmresorts.com/en.html");
+        driver.findElement(By.id("nav-casino-4")).click();
+        WebElement img1 = driver.findElement(By.xpath("(//*[@class = 'comparison-image'] //img)[2]"));
+        String src1 = img1.getAttribute("src");
+        WebElement img2 = driver.findElement(By.xpath("(//*[@class = 'comparison-image'] //img)[3]"));
+        String src2 = img2.getAttribute("src");
+        if (src1.equals(src2)) {
+            return false;
+        }
+        return true;
+    }
+    public static boolean image2(WebDriver driver) {
+        driver.get("https://www.mgmresorts.com/en.html");
+        driver.findElement(By.id("nav-casino-4")).click();
+        WebElement img1 = driver.findElement(By.xpath("(//*[@class = 'comparison-image'] //img)[3]"));
+        String src1 = img1.getAttribute("src");
+        WebElement img2 = driver.findElement(By.xpath("(//*[@class = 'comparison-image'] //img)[4]"));
+        String src2 = img2.getAttribute("src");
+        if (src1.equals(src2)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean image3(WebDriver driver) {
+        driver.get("https://www.mgmresorts.com/en.html");
+        driver.findElement(By.id("nav-casino-4")).click();
+        WebElement img1 = driver.findElement(By.xpath("(//*[@class = 'comparison-image'] //img)[4]"));
+        String src1 = img1.getAttribute("src");
+        WebElement img2 = driver.findElement(By.xpath("(//*[@class = 'comparison-image'] //img)[5]"));
+        String src2 = img2.getAttribute("src");
+        if (src1.equals(src2)) {
+            return false;
+        }
+        return true;
+
+    }
+
 }
